a386922518cc55b5fbdb466cc7804da5cb395eed
Tobias Schutter <tschutter@gmail.com>
2016-09-15 14:51:10 +0200

    Minor

--- classes/Settings/Column/Section/ColumnType.php
+++ classes/Settings/Column/Section/ColumnType.php
@@ -9,9 +9,12 @@ class AC_Settings_Column_Section_ColumnType extends AC_Settings_Column_Section {
 	public function __construct() {
 		parent::__construct();
 
+		$type = ''; //$column->get_type();
+		$name = ''; //$column->get_name();
+
 		$this->set_name( 'label' );
 		$this->set_label( __( 'Type', 'codepress-admin-columns' ) );
-		$this->set_description( __( 'Choose a column type.', 'codepress-admin-columns' ) . '<em>' . __( 'Type', 'codepress-admin-columns' ) . ': ' . $column->get_type() . '</em><em>' . __( 'Name', 'codepress-admin-columns' ) . ': ' . $column->get_name() . '</em>' );
+		$this->set_description( __( 'Choose a column type.', 'codepress-admin-columns' ) . '<em>' . __( 'Type', 'codepress-admin-columns' ) . ': ' . $type . '</em><em>' . __( 'Name', 'codepress-admin-columns' ) . ': ' . $name . '</em>' );
 
 		$field = new AC_Settings_Column_Field( 'type' );
 
