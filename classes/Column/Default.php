d659cf194cd8c31b28267a4b0e8bd3447dd1d8e6
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:07 +0200

    Code consistency changes for classes/Column/Default.php

--- classes/Column/Default.php
+++ classes/Column/Default.php
@@ -6,7 +6,7 @@ class AC_Column_Default extends AC_Column_OriginalAbstract {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-default';
+		$this->properties['type']  = 'column-default';
 		$this->properties['group'] = __( 'Default', 'codepress-admin-columns' );
 	}
 
