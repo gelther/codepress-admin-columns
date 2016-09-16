703594273a043fbeba0963450748dbd1b6a3053a
gelther <lpw.sormano@gmail.com>
2016-09-16 11:03:31 +0200

    Code consistency changes for classes/Column/Post/Modified.php

--- classes/Column/Post/Modified.php
+++ classes/Column/Post/Modified.php
@@ -9,20 +9,19 @@ class AC_Column_Post_Modified extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type']	 	= 'column-modified';
-		$this->properties['label']	 	= __( 'Last modified', 'codepress-admin-columns' );
+		$this->properties['type']  = 'column-modified';
+		$this->properties['label'] = __( 'Last modified', 'codepress-admin-columns' );
 
 		$this->default_options['date_format'] = '';
 	}
 
 	public function get_value( $post_id ) {
-		$modified = $this->get_raw_value( $post_id );
+		$modified    = $this->get_raw_value( $post_id );
 		$date_format = $this->get_option( 'date_format' );
 
 		if ( ! $date_format ) {
 			$value = ac_helper()->date->date( $modified ) . ' ' . ac_helper()->date->time( $modified );
-		}
-		else {
+		} else {
 			$value = date_i18n( $date_format, strtotime( $modified ) );
 		}
 
