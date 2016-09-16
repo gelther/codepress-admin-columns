f33216d1dcc89e625c7191e2a3782da734b276cd
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:58 +0200

    Code consistency changes for classes/Column/Plugin.php

--- classes/Column/Plugin.php
+++ classes/Column/Plugin.php
@@ -6,7 +6,7 @@ class AC_Column_Plugin extends AC_Column_OriginalAbstract {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-plugin';
+		$this->properties['type']  = 'column-plugin';
 		$this->properties['group'] = __( 'Plugins', 'codepress-admin-columns' );
 	}
 
