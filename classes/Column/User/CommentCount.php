d39059a11a7bea0815df07619d68cd26027f05bb
gelther <lpw.sormano@gmail.com>
2016-09-16 11:04:06 +0200

    Code consistency changes for classes/Column/User/CommentCount.php

--- classes/Column/User/CommentCount.php
+++ classes/Column/User/CommentCount.php
@@ -9,7 +9,7 @@ class AC_Column_User_CommentCount extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-user_commentcount';
+		$this->properties['type']  = 'column-user_commentcount';
 		$this->properties['label'] = __( 'Comment Count' );
 	}
 
