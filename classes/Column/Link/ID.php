316d76eb48187d1d69f4f0d7d67c734cb39e9f57
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:11 +0200

    Code consistency changes for classes/Column/Link/ID.php

--- classes/Column/Link/ID.php
+++ classes/Column/Link/ID.php
@@ -9,8 +9,8 @@ class AC_Column_Link_ID extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type']	 	= 'column-link_id';
-		$this->properties['label']	 	= __( 'ID', 'codepress-admin-columns' );
+		$this->properties['type']  = 'column-link_id';
+		$this->properties['label'] = __( 'ID', 'codepress-admin-columns' );
 	}
 
 	function get_value( $id ) {
