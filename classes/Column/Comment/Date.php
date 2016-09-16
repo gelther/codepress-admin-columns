53ed1130922693b1dab019a810e533926eb1b91f
gelther <lpw.sormano@gmail.com>
2016-09-16 11:01:42 +0200

    Code consistency changes for classes/Column/Comment/Date.php

--- classes/Column/Comment/Date.php
+++ classes/Column/Comment/Date.php
@@ -9,7 +9,7 @@ class AC_Column_Comment_Date extends AC_Column_DateAbstract {
 	public function init() {
 		parent::init();
 
-		$this->default_options['width'] = 14;
+		$this->default_options['width']      = 14;
 		$this->default_options['width_unit'] = '%';
 	}
 
