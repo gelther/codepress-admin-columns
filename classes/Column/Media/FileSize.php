b563a9e3e45dcac1efbbc0b7916b8c57889b425d
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:42 +0200

    Code consistency changes for classes/Column/Media/FileSize.php

--- classes/Column/Media/FileSize.php
+++ classes/Column/Media/FileSize.php
@@ -9,7 +9,7 @@ class AC_Column_Media_FileSize extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-file_size';
+		$this->properties['type']  = 'column-file_size';
 		$this->properties['label'] = __( 'File size', 'codepress-admin-columns' );
 	}
 
@@ -17,7 +17,7 @@ class AC_Column_Media_FileSize extends CPAC_Column {
 		$value = '';
 
 		$file = wp_get_attachment_url( $id );
-		$abs = str_replace( WP_CONTENT_URL, WP_CONTENT_DIR, $file );
+		$abs  = str_replace( WP_CONTENT_URL, WP_CONTENT_DIR, $file );
 
 		if ( file_exists( $abs ) ) {
 			$value = $this->get_readable_filesize( filesize( $abs ) );
@@ -36,7 +36,7 @@ class AC_Column_Media_FileSize extends CPAC_Column {
 	 * @return string Readable filesize
 	 */
 	function get_readable_filesize( $size ) {
-		$filesizename = array( "Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" );
+		$filesizename = array( 'Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' );
 
 		return $size ? round( $size / pow( 1024, ( $i = floor( log( $size, 1024 ) ) ) ), 2 ) . ' ' . $filesizename[ $i ] : '0 Bytes';
 	}
