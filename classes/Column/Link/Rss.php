b56ae165278cbb20557ab0cd070b1ec815a2c04a
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:18 +0200

    Code consistency changes for classes/Column/Link/Rss.php

--- classes/Column/Link/Rss.php
+++ classes/Column/Link/Rss.php
@@ -9,7 +9,7 @@ class AC_Column_Link_Rss extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-rss';
+		$this->properties['type']  = 'column-rss';
 		$this->properties['label'] = __( 'Rss', 'codepress-admin-columns' );
 	}
 
