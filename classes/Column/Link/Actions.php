364f8845b64608762ddc4af98f257575978cf71e
gelther <lpw.sormano@gmail.com>
2016-09-16 11:02:08 +0200

    Code consistency changes for classes/Column/Link/Actions.php

--- classes/Column/Link/Actions.php
+++ classes/Column/Link/Actions.php
@@ -9,7 +9,7 @@ class AC_Column_Link_Actions extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-actions';
+		$this->properties['type']  = 'column-actions';
 		$this->properties['label'] = __( 'Actions', 'codepress-admin-columns' );
 	}
 
@@ -35,8 +35,8 @@ class AC_Column_Link_Actions extends CPAC_Column {
 
 		$edit_link = get_edit_bookmark_link( $link );
 
-		$actions['edit'] = '<a href="' . $edit_link . '">' . __( 'Edit' ) . '</a>';
-		$actions['delete'] = "<a class='submitdelete' href='" . wp_nonce_url( "link.php?action=delete&amp;link_id=$link->link_id", 'delete-bookmark_' . $link->link_id ) . "' onclick=\"if ( confirm( '" . esc_js( sprintf( __( "You are about to delete this link '%s'\n  'Cancel' to stop, 'OK' to delete." ), $link->link_name ) ) . "' ) ) { return true;}return false;\">" . __( 'Delete' ) . "</a>";
+		$actions['edit']   = '<a href="' . $edit_link . '">' . __( 'Edit' ) . '</a>';
+		$actions['delete'] = "<a class='submitdelete' href='" . wp_nonce_url( "link.php?action=delete&amp;link_id=$link->link_id", 'delete-bookmark_' . $link->link_id ) . "' onclick=\"if ( confirm( '" . esc_js( sprintf( __( "You are about to delete this link '%s'\n  'Cancel' to stop, 'OK' to delete." ), $link->link_name ) ) . "' ) ) { return true;}return false;\">" . __( 'Delete' ) . '</a>';
 
 		return implode( ' | ', $actions );
 	}
