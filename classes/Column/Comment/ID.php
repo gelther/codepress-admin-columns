a0402980975aafbe1d833b65e70092f0a5bf3308
gelther <lpw.sormano@gmail.com>
2016-09-16 11:01:47 +0200

    Code consistency changes for classes/Column/Comment/ID.php

--- classes/Column/Comment/ID.php
+++ classes/Column/Comment/ID.php
@@ -9,11 +9,12 @@ class AC_Column_Comment_ID extends CPAC_Column {
 	public function init() {
 		parent::init();
 
-		$this->properties['type'] = 'column-comment_id';
+		$this->properties['type']  = 'column-comment_id';
 		$this->properties['label'] = __( 'ID', 'codepress-admin-columns' );
 	}
 
 	public function get_value( $id ) {
 		return $id;
 	}
+
 }
